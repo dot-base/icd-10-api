adba8ddd0f49572a0f6f67a5bfd862bb1e7ec6a1
Andrea Kreichgauer <andrea@kreichgauer.com>
2020-08-12 15:18:22 +0200

    loading and formatting icd10-data at compile time

--- src/model/fhirR4Codesystem.ts
+++ src/model/fhirR4Codesystem.ts
@@ -4,15 +4,41 @@ import icd10gm from "data/codesystem_icd10_gm_2020.json";
 
 export class ICD10gm {
   public static codesystem: R4.ICodeSystem | undefined;
+  public static codesystemPrefilteredText: R4.ICodeSystem | undefined;
   public static codesystemFailed: io.Errors | undefined;
+  public static test: R4.ICodeSystem;
 
-  public static initCodesystem() {
+  public static initCodesystem(): boolean {
     const icd10gmJson: R4.ICodeSystem = JSON.parse(JSON.stringify(icd10gm));
     const icd10gmDecoded = R4.RTTI_CodeSystem.decode(icd10gmJson);
-
-    ICD10gm.test = <R4.ICodeSystem>icd10gmDecoded.value;
-    ICD10gm.codesystem = icd10gmDecoded.isRight() ? <R4.ICodeSystem>icd10gmDecoded.value : undefined;
+    ICD10gm.codesystem = icd10gmDecoded.isRight()
+      ? <R4.ICodeSystem>icd10gmDecoded.value
+      : undefined;
     ICD10gm.codesystemFailed = ICD10gm.codesystem ? undefined : <io.Errors>icd10gmDecoded.value;
+    return ICD10gm.codesystem ? true : false;
   }
 
+  /**
+   * removes all entries of other kind than "category" , like "chapter" or "block", only specific ICD10 codes remain
+   * within remaining icd10 entries: remove all properties with property.code other than "inclusion"
+   * removes non aplha-numeric characters (like -) and spaces between words from field "display"
+   * */
+  public static prefilterCodesystemForTextSearch(): boolean {
+    ICD10gm.codesystemPrefilteredText = JSON.parse(JSON.stringify(ICD10gm.codesystem));
+
+    if (ICD10gm.codesystemPrefilteredText?.concept) {
+      ICD10gm.codesystemPrefilteredText.concept = ICD10gm.codesystemPrefilteredText.concept.filter(
+        (elem) => {
+          return elem.property?.some((prop) =>
+            prop.valueCode ? prop.valueCode === "category" && prop.code === "kind" : false
+          );
+        }
+      );
+      ICD10gm.codesystemPrefilteredText.concept.forEach((elem) => {
+        elem.display = elem.display?.replace(/[^0-9A-ZÄÖÜ]/gi, "");
+        elem.property = elem.property?.filter((prop) => prop.code === "inclusion");
+      });
+    }
+    return true;
+  }
 }
--- src/routes/hello.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router: express.Router = express.Router();
-
-router.get("/", async (req: express.Request, res: express.Response) => {
-  res.send("hello");
-});
-
-export default router;
