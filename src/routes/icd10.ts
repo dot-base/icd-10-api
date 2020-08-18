6cf310ed8f5037d86016d24ebef985ae81f79b05
Andrea <andrea@kreichgauer.com>
2020-08-18 15:29:13 +0200

    refactoring - changed static to instance based context

--- src/routes/icd10.ts
+++ src/routes/icd10.ts
@@ -4,14 +4,15 @@ import * as icd10 from "../controller/icd10Controller";
 const router: express.Router = express.Router();
 
 router.get("/", async (req: express.Request, res: express.Response) => {
-  try {
-    const str = JSON.parse(JSON.stringify(req.query));
-    const icd10Res: string | JSON = await icd10.getFiltered(str.search ? str.search : "");
-
-    res.status(200).send(icd10Res);
-  } catch (e) {
-    res.status(500).send(e.message);
-  }
+  const str = JSON.parse(JSON.stringify(req.query));
+  if (str.search) {
+    try {
+      const icd10Res: JSON = await icd10.getFiltered(str.search);
+      res.status(200).send(icd10Res);
+    } catch (e) {
+      res.status(500).send(e.message);
+    }
+  } else res.status(400).send("no search params defined");
 });
 
 export default router;
--- src/services/textFilter.ts
+++ src/services/textFilter.ts
@@ -1,49 +1,53 @@
-import * as r4Codesystem from "../model/icd10gmCodesystem";
 import Fuse from "fuse.js";
 import { ICodeSystem_Concept } from "@ahryman40k/ts-fhir-types/lib/R4";
 import { Filter } from "./filter";
 
-export class TextFilter extends Filter {
-    private static query: Fuse.Expression[] = [];
-    private static keys: Fuse.FuseOptionKeyObject[] = [
+class TextFilter extends Filter {
+  query: Fuse.Expression[] = [];
+  keys: Fuse.FuseOptionKeyObject[] = [
         { name: "display", weight: 0.6 },
         { name: "property.valueString", weight: 0.4 },
     ];
 
-    static search(searchTerms: string[]): Fuse.FuseResult<ICodeSystem_Concept>[] {
-        let res: Fuse.FuseResult<ICodeSystem_Concept>[] = [];
-        let termCount = searchTerms.length;
-        const termCombs: string[][] = TextFilter.getTermCombinations(searchTerms);
-
-        while (res.length < 1 && termCount > 0) {
-            const termsByLength: string[][] = termCombs.filter((terms) => terms.length == termCount);
-            TextFilter.setMultipleTermQuery(termsByLength);
-            res = Filter.doSearch(TextFilter.keys, TextFilter.query);
-            termCount--;
-        }
-        return res;
+  search(searchTerms: string[]): Fuse.FuseResult<ICodeSystem_Concept>[] {
+    let res: Fuse.FuseResult<ICodeSystem_Concept>[] = [];
+    let termCount = searchTerms.length;
+    const termCombs: string[][] = this.getCombinations(searchTerms);
+    while (res.length < 1 && termCount > 0) {
+      const termsByLength: string[][] = termCombs.filter((terms) => terms.length == termCount);
+      this.setMultipleTermsQuery(termsByLength);
+      res = this.doSearch(this.keys, this.query);
+      termCount--;
     }
+    return res;
+  }
 
-    static getTermCombinations(terms: string[]): string[][] {
-        if (terms.length === 1) return [terms];
-        else {
-            const subarr: string[][] = TextFilter.getTermCombinations(terms.slice(1));
-            return subarr.concat(
-                subarr.map((e) => e.concat([terms[0]])),
-                [[terms[0]]]
-            );
-        }
+  private getCombinations(terms: string[]): string[][] {
+    if (terms.length === 1) return [terms];
+    else {
+      const subarr: string[][] = this.getCombinations(terms.slice(1));
+      return subarr.concat(
+        subarr.map((e) => e.concat([terms[0]])),
+        [[terms[0]]]
+      );
     }
+  }
 
-    static setQuery(queryStr: string[]) {
+  setQuery(queryStr: string[]): void {
         queryStr.forEach((str) => TextFilter.query.push({ display: str }, { "property.valueString": str }));
     }
 
-    static setMultipleTermQuery(searchTerms: string[][]) {
-        const queryStr: string[] = [];
+  private setMultipleTermsQuery(searchTerms: string[][]): void {
+    const queryStr: string[] = [];
+    };
         searchTerms.forEach((term) => {
             queryStr.push(Filter.getQueryStringFuzzyMatchAND(term));
         });
-        TextFilter.setQuery(queryStr);
-    }
+    this.setQuery(queryStr);
+  }
 }
+
+export const initSearch = (terms: string[]): Fuse.FuseResult<ICodeSystem_Concept>[] => {
+  const textFilter = new TextFilter();
+  return textFilter.search(terms);
+};
