690f232e8ce6cd0e45a2f7162de5b41bdd915fe5
Andrea <andrea@kreichgauer.com>
2020-08-21 16:36:57 +0200

    splitted codesystem preprocessing into several methods

--- src/model/ICD10gmCodesystem.ts
+++ src/model/ICD10gmCodesystem.ts
@@ -2,24 +2,22 @@ import { R4 } from "@ahryman40k/ts-fhir-types";
 import * as io from "io-ts";
 import icd10gm from "data/codesystem_icd10_gm_2020.json";
 
-export class ICD10gm {
-  private static instance: ICD10gm;
-  public static codesystem: R4.ICodeSystem | undefined;
-  public static processedCodesystem: R4.ICodeSystem | undefined;
+export default class ICD10gm {
+  public static instance: ICD10gm;
+  public codesystem: R4.ICodeSystem;
+  public processedCodesystem: R4.ICodeSystem;
 
-  private ICD10gm() {
-    ICD10gm.codesystem = ICD10gm.initCodesystem();
-    ICD10gm.processedCodesystem = ICD10gm.preProcessCodeSystem();
+  private constructor() {
+    this.codesystem = this.initCodesystem();
+    this.processedCodesystem = this.preProcessCodeSystem();
   }
 
   public static getInstance(): ICD10gm {
-    if (!ICD10gm.instance) {
-      ICD10gm.instance = new ICD10gm();
-    }
+    if (!ICD10gm.instance) ICD10gm.instance = new ICD10gm();
     return ICD10gm.instance;
   }
 
-  private static initCodesystem(): R4.ICodeSystem {
+  private initCodesystem(): R4.ICodeSystem {
     const icd10gmDecoded = R4.RTTI_CodeSystem.decode(icd10gm);
 
     if (!icd10gmDecoded.isRight())
@@ -30,32 +28,45 @@ export class ICD10gm {
   }
 
   /**
-   * removes all entries of other kind than "category" , like "chapter" or
-   * block", only specific ICD10 codes remain.
-   * Removes non aplha-numeric characters between words from field "display"
-   * */
-  private static preProcessCodeSystem(): R4.ICodeSystem {
-    const processedCodesystem: R4.ICodeSystem = JSON.parse(JSON.stringify(ICD10gm.codesystem));
-    const stripRegex = new RegExp("[^0-9A-ZÄÖÜ]", "gi");
+   * Removes all entries of other kind than "category" , like "chapter" or "block". Only specific ICD10 codes remain
+   * Copies fields "display" and "inclusion" to extensions and removes all non-alphanumeric characters
+   **/
+  private preProcessCodeSystem(): R4.ICodeSystem {
+    const processedCodesystem: R4.ICodeSystem = JSON.parse(JSON.stringify(this.codesystem));
 
-    if (!processedCodesystem.concept) throw new Error("Preprocessing ICD10 codesystem failed");
+    if (!processedCodesystem.concept)
+      throw new Error("Initializing ICD10 codesystem from JSON failed");
 
     processedCodesystem.concept = processedCodesystem.concept.filter((elem) => {
-      return elem.property?.some((prop) =>
-        prop.valueCode ? prop.valueCode === "category" && prop.code === "kind" : false
-      );
+      return elem.property ? this.isTypeICDCode(elem.property) : false;
     });
-    processedCodesystem.concept.forEach((elem) => {
-      elem.extension = [{ id: "displayCopy", valueString: elem.display?.replace(stripRegex, "") }];
+
+    processedCodesystem.concept = this.trimAndCopySearchFields(processedCodesystem.concept);
+
+    return processedCodesystem;
+  }
+
+  private isTypeICDCode(property: R4.ICodeSystem_Property1[]) {
+    return property.some((prop) =>
+      prop.valueCode ? prop.valueCode === "category" && prop.code === "kind" : false
+    );
+  }
+
+  private trimAndCopySearchFields(concept: R4.ICodeSystem_Concept[]): R4.ICodeSystem_Concept[] {
+    const trimRegex = new RegExp("[^0-9A-ZÄÖÜ]", "gi");
+
+    concept.forEach((elem) => {
+      elem.extension = [{ id: "displayCopy", valueString: elem.display?.replace(trimRegex, "") }];
+
       elem.property?.forEach((prop) => {
         if (prop.code === "inclusion") {
           elem.modifierExtension = [
-            { id: "InclusionCopy", valueString: prop.valueString?.replace(stripRegex, "") },
+            { id: "InclusionCopy", valueString: prop.valueString?.replace(trimRegex, "") },
           ];
         }
       });
     });
 
-    return processedCodesystem;
+    return concept;
   }
 }
