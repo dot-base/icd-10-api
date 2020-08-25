7c53345d93d94e1ff02234a49cb1b3f1d5cbc48b
Andrea <andrea@kreichgauer.com>
2020-08-25 19:32:34 +0200

    changed icdController to static context

--- src/controller/icd10Controller.ts
+++ src/controller/icd10Controller.ts
@@ -3,31 +3,29 @@ import { ICodeSystem_Concept } from "@ahryman40k/ts-fhir-types/lib/R4";
 import { CodeFilter } from "@/services/codeFilter";
 import { TextFilter } from "@/services/textFilter";
 
-export const getFiltered = async (searchstring: string): Promise<JSON> => {
-  const icd10Controller = new ICD10Controller();
+export class ICD10Controller {
+  private static icdRegex = new RegExp("[A-TV-Z][0-9][0-9].?[0-9A-TV-Z]{0,4}", "i");
+  private static stripRegex = new RegExp("[ -]+");
 
-  const searchTerms: string[] = icd10Controller.stripTerms(searchstring);
-  const icd10Codes: string[] = icd10Controller.filterCodes(searchTerms);
+  public static getFiltered(searchstring: string): Fuse.FuseResult<ICodeSystem_Concept>[] {
+    const searchTerms: string[] = ICD10Controller.splitTerms(searchstring);
+    const icd10Codes: string[] = ICD10Controller.filterCodes(searchTerms);
 
-  let response: Fuse.FuseResult<ICodeSystem_Concept>[] = [];
-  if (icd10Codes.length >= 1) response = CodeFilter.initSearch(icd10Codes);
-  if (response.length < 1) response = TextFilter.initSearch(searchTerms);
-  return JSON.parse(JSON.stringify(response));
-};
-
-class ICD10Controller {
-  private icdRegex = new RegExp("[A-TV-Z][0-9][0-9].?[0-9A-TV-Z]{0,4}", "i");
-  private stripRegex = new RegExp("[ -]+");
-
-  private isICD10Code(str: string): boolean {
-    return this.icdRegex.test(str);
+    let response: Fuse.FuseResult<ICodeSystem_Concept>[] = [];
+    if (icd10Codes.length >= 1) response = CodeFilter.initSearch(icd10Codes);
+    if (response.length < 1) response = TextFilter.initSearch(searchTerms);
+    return response;
   }
 
-  public filterCodes(terms: string[]): string[] {
-    return terms.filter((term) => this.isICD10Code(term));
+  private static isICD10Code(str: string): boolean {
+    return ICD10Controller.icdRegex.test(str);
   }
 
-  public stripTerms(str: string): string[] {
-    return str.split(this.stripRegex);
+  private static filterCodes(terms: string[]): string[] {
+    return terms.filter((term) => ICD10Controller.isICD10Code(term));
+  }
+
+  private static splitTerms(str: string): string[] {
+    return str.split(ICD10Controller.stripRegex);
   }
 }
