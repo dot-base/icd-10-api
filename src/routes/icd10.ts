ac83c0e40cea5a735d401b3532a1cf054dba960f
Andrea <andrea@kreichgauer.com>
2020-08-25 19:33:41 +0200

    minor changes from review request

--- src/routes/icd10.ts
+++ src/routes/icd10.ts
@@ -1,18 +1,18 @@
 import express from "express";
-import * as icd10 from "../controller/icd10Controller";
+import { ICD10Controller } from "@/controller/icd10Controller";
 
 const router: express.Router = express.Router();
 
 router.get("/", async (req: express.Request, res: express.Response) => {
-  const str = JSON.parse(JSON.stringify(req.query));
-  if (str.search) {
+  if (req.query.search) {
     try {
-      const icd10Res: JSON = await icd10.getFiltered(str.search);
-      res.status(200).send(icd10Res);
+      const icd10Res = ICD10Controller.getFiltered(req.query.search as string);
+      return res.status(200).send(icd10Res);
     } catch (e) {
-      res.status(500).send(e.message);
+      return res.status(500).send(e.message);
     }
-  } else res.status(400).send("no search params defined");
+  }
+  return res.status(400).send("no query component 'search' present").end();
 });
 
 export default router;
