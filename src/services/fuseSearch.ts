3449caea21b51b74a30267b9d5cbd37d6aeb2850
Andrea <andrea@kreichgauer.com>
2020-08-28 10:29:59 +0200

    Revert "add comment on setQueryString for readibility"
    This reverts commit 370dea12c7cdc3cc0cd7e981f41093e35ed6ee0e.

--- src/services/fuseSearch.ts
+++ src/services/fuseSearch.ts
@@ -4,9 +4,6 @@ import { ICodeSystem_Concept } from "@ahryman40k/ts-fhir-types/lib/R4";
 import { QueryOptions } from "@/types/queryOptions";
 
 export default class FuseSearch {
-  /**Builds fuse search query for single and multiple term queries.
-   * Thus, initialization of queryStr is handled seperately and loop starts at index 1.
-   * */
   protected static getQueryString(searchTerms: string[], queryOptions: QueryOptions): string {
     let queryStr = queryOptions.matchType + searchTerms[0];
     for (let i = 1; i < searchTerms.length; i++)
