9063e1ad8037ea52b2063ca9edccd43390b9ddc6
Christian Friedow <christian@friedow.com>
2020-12-10 16:20:44 +0100

    Refactor server ts

--- src/server.ts
+++ src/server.ts
@@ -3,46 +3,58 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import * as Sentry from "@sentry/node";
 import * as Tracing from "@sentry/tracing";
+
 import icd10Router from "@/routes/icd10";
 import ICD10gm from "@/model/ICD10gmCodesystem";
 
-async function startApiServer(): Promise<void> {
-  const app: express.Application = express();
-  const port: string = process.env.PORT || "3000";
-
-  if (process.env.SENTRY_DSN) {
-    Sentry.init({
-      dsn: process.env.SENTRY_DSN,
-      integrations: [
-        new Sentry.Integrations.Http({ tracing: true }),
-        new Tracing.Integrations.Express({ app }),
-      ],
-      tracesSampleRate: 1.0,
-    });
-
-    app.use(Sentry.Handlers.requestHandler());
-    app.use(Sentry.Handlers.tracingHandler());
+class Icd10Api {
+  private static get port(): string {
+    return process.env.PORT || "3000";
   }
 
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-  app.use(cors());
+  private static get sentryIsEnabled(): boolean {
+    return !!process.env.SENTRY_DSN && !!process.env.SENTRY_ENVIRONMENT;
+  }
 
-  try {
+  private async startApiServer() {
+    const app: express.Application = express();
+
+    if (Icd10Api.sentryIsEnabled) {
+      Sentry.init({
+        dsn: process.env.SENTRY_DSN,
+        integrations: [
+          new Sentry.Integrations.Http({ tracing: true }),
+          new Tracing.Integrations.Express({ app }),
+        ],
+        tracesSampleRate: 1.0,
+        environment: process.env.SENTRY_ENVIRONMENT,
+      });
+
+      app.use(Sentry.Handlers.requestHandler());
+      app.use(Sentry.Handlers.tracingHandler());
+    }
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(cors());
+
     ICD10gm.getInstance();
     console.log("Loading and prefiltering ICD10gm Codesystem succeded");
-  } catch (error) {
-    throw error;
+
+    app.use("/api/icd10", icd10Router);
+
+    if (Icd10Api.sentryIsEnabled) {
+      app.use(Sentry.Handlers.errorHandler());
+    }
+
+    app.listen(Icd10Api.port, () => {
+      console.log(`Server listening on ${Icd10Api.port}`);
+    });
   }
 
-  app.use("/api/icd10", icd10Router);
-
-  if (process.env.SENTRY_DSN) {
-    app.use(Sentry.Handlers.errorHandler());
+  constructor() {
+    this.startApiServer();
   }
-
-  app.listen(port);
-  console.log(`Server is listening on port ${port}`);
 }
 
-startApiServer();
+new Icd10Api();
