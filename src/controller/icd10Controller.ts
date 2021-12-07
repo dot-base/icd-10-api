dot-base/icd-10-api

commit 7322fbcdbc77783d008d7665541d3684d7c75b10

fix(api)!: remove tmp extensions from results
Remove extensions added in ICD10gm.preProcessCodeSystem()

ICD10gm.preProcessCodeSystem() adds extensions to the FHIR objects in
order to facilitate better search results. Those extensions are invalid
according to the FHIR standard though (not having an .url field). Therefor
the extensions need to be removed before delivery of the search results.

Fixes https://github.com/dot-base/icd-10-api/issues/24

BREAKING CHANGE: removes all extensions and modifierExtensions from API
results, that is 'displayCopy' and 'InclusionCopy'.

src/controller/icd10Controller.ts
@@ -15,7 +15,8 @@ export class ICD10Controller {
       const codeResponse = CodeFilter.initSearch(icd10Codes);
       if (codeResponse.length > 0) return codeResponse;
     }
-    return TextFilter.initSearch(searchTerms);
+
+    return ICD10Controller.removeExtensions(TextFilter.initSearch(searchTerms));
   }
 
   private static isICD10Code(str: string): boolean {
@@ -28,5 +29,22 @@ export class ICD10Controller {
 
   private static splitTerms(str: string): string[] {
     return str.split(ICD10Controller.stripRegex);
+  }
+
+  /**
+   * Remove extensions added in ICD10gm.preProcessCodeSystem()
+   *
+   * ICD10gm.preProcessCodeSystem() adds extensions to the FHIR objects in
+   * order to facilitate better search results. Those extensions are invalid
+   * according to the FHIR standard though (not having an .url field). Therefor
+   * the extensions need to be removed before delivery of the search results.
+   *
+   * Fixes https://github.com/dot-base/icd-10-api/issues/24
+   */
+  private static removeExtensions(
+    res: Fuse.FuseResult<ICodeSystem_Concept>[]
+  ): Fuse.FuseResult<ICodeSystem_Concept>[] {
+    res.forEach((r) => (r.item.extension = r.item.modifierExtension = undefined));
+    return res;
   }
 }

commit 3974ea593776091fb26a6d3b912d5ff80de42349

fix: regression: db-corruption
This fixes a db corruption bug introduced with 7322fbcdbc77783d008d7665541d3684d7c75b10. This closes #27.

src/controller/icd10Controller.ts
@@ -16,7 +16,12 @@ export class ICD10Controller {
       if (codeResponse.length > 0) return codeResponse;
     }
 
-    return ICD10Controller.removeExtensions(TextFilter.initSearch(searchTerms));
+    const searchResult = TextFilter.initSearch(searchTerms);
+
+    // copy the results before removing extensions, otherwise
+    // we would change the actual database we are searching on
+    const searchResultCopy = JSON.parse(JSON.stringify(searchResult));
+    return ICD10Controller.removeExtensions(searchResultCopy);
   }
 
   private static isICD10Code(str: string): boolean {