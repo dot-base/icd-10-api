1a76d3e8cb23a7ce82543bef35697ddd6157dfb0
rti <rti@users.noreply.github.com>
2022-02-21 11:26:51 +0100

    feat: simple console logging
    switches to simple console logging, removes winston dependency

--- src/logger.ts
+++ src/logger.ts
@@ -1,27 +1,4 @@
-import winston from "winston";
 
-const logger: winston.Logger = createLogger();
-
-function createLogger() {
-  const transportOptions: winston.transport[] = [
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    }),
-  ];
-  if (process.env.NODE_ENV !== "development") {
-    transportOptions.push(
-      new winston.transports.File({
-        filename: "logs/error.log",
-        level: "error",
-      }),
-      new winston.transports.File({ filename: "logs/info.log", level: "info" }),
-      new winston.transports.File({ filename: "logs/all.log" })
-    );
-  }
-
-  return winston.createLogger({
-    transports: transportOptions,
-  });
-}
+const logger = console;
 
 export default logger;
