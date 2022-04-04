16f0b391eb43ff4feb2d5c3591de67de9cf1aa77
Andrea <andrea@kreichgauer.com>
2022-04-05 00:31:35 +0200

    refactor icd10 router

--- src/routers/icd10.ts
+++ src/routers/icd10.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import { ICD10Controller } from "@/controller/icd10Controller";
+import HTTPError from "@/utils/HTTPError";
 
 const router: express.Router = express.Router();
 
 router.get("/", async (req: express.Request, res: express.Response) => {
-  if (req.query.search) {
-    try {
-      const icd10Res = ICD10Controller.getFiltered(req.query.search as string);
-      return res.status(200).send(icd10Res);
-    } catch (e) {
-      return res.status(500).send(e.message);
-    }
+  if (!req.query.search) 
+    return res.status(400).send("Request is missing a query parameter 'search'.").end();
+  
+  try {
+    const icd10Codes = ICD10Controller.getFiltered(req.query.search as string);
+    return res.status(200).send(icd10Codes);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    if(e instanceof HTTPError) 
+      res.status(e.status).send(e.message);
+    else
+      res.status(500).send(e.message);
   }
-  return res.status(400).send("no query component 'search' present").end();
+  
 });
 
 export default router;
---
+++ src/utils/HttpError.ts
@@ -0,0 +1,8 @@
+export default class HTTPError extends Error {
+    public status: number;
+
+    constructor (message: string, status: number) {
+        super(message);
+        this.status = status;
+    }
+}
