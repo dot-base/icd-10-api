ad59121436c0ee84bb7685ece3e984be09829e54
andreakreichgauer <38720222+andreakreichgauer@users.noreply.github.com>
2022-04-07 13:18:30 +0200

    fix: endless search for multiple-term queries
    This PR fixes "endless" searches or time-outs due to long multiple-term queries. This closes #38 .

---
+++ src/express.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import ICD10Router from "@/routers/icd10";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/icd10", ICD10Router);
+
+export default app;
