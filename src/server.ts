9059e77b921991fd80b065acfabbc3b243165892
rti <rti@users.noreply.github.com>
2022-05-17 09:52:42 +0200

    feat: remove sentry
    feat: remove sentry

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,10 @@
-import { Express } from "express";
 import express from "@/express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import * as Sentry from "@sentry/node";
-import * as Tracing from "@sentry/tracing";
 
 export default class Server {
   private static get port(): string {
     return process.env.PORT || "3000";
-  }
-
-  private static get sentryIsEnabled(): boolean {
-    return !!process.env.SENTRY_DSN && !!process.env.SENTRY_ENVIRONMENT;
-  }
-
-  private static enableSentry(app: Express) {
-    if (!Server.sentryIsEnabled) return;
-    Sentry.init({
-      dsn: process.env.SENTRY_DSN,
-      integrations: [
-        new Sentry.Integrations.Http({ tracing: true }),
-        new Tracing.Integrations.Express({ app }),
-      ],
-      tracesSampleRate: 1.0,
-      environment: process.env.SENTRY_ENVIRONMENT,
-    });
-
-    app.use(Sentry.Handlers.requestHandler());
-    app.use(Sentry.Handlers.tracingHandler());
-    app.use(Sentry.Handlers.errorHandler());
   }
 
   private static setDefaultEnvironmentVariables() {
@@ -45,7 +21,6 @@ export default class Server {
     express.use(cors());
 
     Server.setDefaultEnvironmentVariables();
-    Server.enableSentry(express);
 
     express.listen(Server.port, () => {
       console.log(`Server is listening on ${Server.port}`);
