c0a4fbeee639e6cd4d8535aa5a31987e5e3b5fd6
velramiir <justus@zeinert.dev>
2023-09-27 13:19:21 +0200

    feat: return only complete codesystem concepts

--- src/routers/icd10.ts
+++ src/routers/icd10.ts
@@ -9,7 +9,7 @@ router.get("/", async (req: express.Request, res: express.Response) => {
     return res.status(400).send("Request is missing a query parameter 'search'.").end();
 
   try {
-    const icd10Codes = ICD10Controller.getFiltered(req.query.search as string);
+    const icd10Codes = ICD10Controller.getCompleteSearchResults(req.query.search as string);
     return res.status(200).send(icd10Codes);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (e: any) {
